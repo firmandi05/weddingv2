4657be128dc854f2f41b460915c72ef03005f7d7
Hairu Firmandi <firmandi05@gmail.com>
2024-02-13 20:02:36 +0700

    add successful copied

---
+++ js/script.js
@@ -0,0 +1,8 @@
+async function copyContent(target) {
+  let rekening = document.getElementById(target).innerText;
+  navigator.clipboard.writeText(rekening);
+  document.getElementById(`${target}-tooltip`).style.display = 'block';
+  setTimeout( function() {
+    document.getElementById(`${target}-tooltip`).style.display = "none";
+}, 1000);
+}
