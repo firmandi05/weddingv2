6831d59373126d5102ec48b98cca257c01a88963
Hairu Firmandi <firmandi05@gmail.com>
2024-02-15 01:38:59 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 
 // Copy to Clipboard
 async function copyContent(target) {
-  let rekening = document.getElementById(target).innerText;
-  navigator.clipboard.writeText(rekening);
+  let rekening = document.getElementById(target);
+  const storage = document.createElement('textarea');
+  storage.value = rekening.innerText;
+  rekening.appendChild(storage);
+  storage.select();
+  storage.setSelectionRange(0,99999);
+  document.execCommand('copy');
+  rekening.removeChild(storage);
   document.getElementById(`${target}-tooltip`).style.display = 'block';
   setTimeout( function() {
     document.getElementById(`${target}-tooltip`).style.display = "none";
 }, 1000);
 }
-
 // navbar
 function myFunction() {
   let x = document.getElementById("myNavbar");
